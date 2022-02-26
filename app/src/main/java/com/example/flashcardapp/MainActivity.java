6a05454b1bdd628c87d6e9f4aa9808ee0c6e003f
pedroamuniz <pedro3nov@gmail.com>
2022-02-26 14:38:38 -0500

    Initial commit

---
+++ app/src/main/java/com/example/flashcardapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.flashcardapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+
+
+}
