ecdc3ef257b302dd5ad9e24ca40a352fef841347
pedroamuniz <pedro3nov@gmail.com>
2022-02-28 15:18:22 -0500

    Multiple choice answers

--- app/src/main/java/com/example/flashcardapp/MainActivity.java
+++ app/src/main/java/com/example/flashcardapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.flashcardapp;
 
+import androidx.annotation.ColorInt;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 
@@ -12,20 +14,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        boolean selected = false;
+
+        View possibleAnswer1 = findViewById(R.id.possible_answer1);
+        View possibleAnswer2 = findViewById(R.id.possible_answer2);
+        View possibleAnswer3 = findViewById(R.id.possible_answer3);
+
         findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
-                findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+//                if (selected){
+//                    possibleAnswer1.setBackgroundColor(getColor(R.color.red));
+//                    pos
+//                }
             }
         });
-        findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener() {
+
+        View.OnClickListener wrongAnswer = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
-                findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
+                view.setBackgroundColor(getColor(R.color.red));
+                possibleAnswer3.setBackgroundColor(getColor(R.color.lime));
             }
-        });
+        };
+
+        View.OnClickListener rightAnswer = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                view.setBackgroundColor(getColor(R.color.lime));
+            }
+        };
+
+        possibleAnswer1.setOnClickListener(wrongAnswer);
+        possibleAnswer2.setOnClickListener(wrongAnswer);
+        possibleAnswer3.setOnClickListener(rightAnswer);
+
+
     }
 
 
